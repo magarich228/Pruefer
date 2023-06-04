125c048ba6bf1406a48a7ad34f1a5b17a447e53c
Kirill <kirillgroshev52@gmail.com>
2023-06-04 23:24:31 +0300

    Комменты.

--- PrueferCode/Program.cs
+++ PrueferCode/Program.cs
@@ -1,54 +1,84 @@
 ﻿using Newtonsoft.Json;
+using System.Text;
 
-Console.WriteLine("Введите путь до файла:");
-
-var path = Console.ReadLine();
-
-//Если по введенному пути файл не найден, то завершаем программу.
-if (string.IsNullOrEmpty(path) || !File.Exists(path))
+internal class Program
 {
-    Console.WriteLine("Файл не найден.");
-    return;
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Введите путь до файла:");
+
+        var path = Console.ReadLine();
+
+        //Если файл не найден, то завершаем выполнение программы.
+        if (string.IsNullOrEmpty(path) || !File.Exists(path))
+        {
+            Console.WriteLine("Файл не найден.");
+            return;
+        }
+
+        //читаем всё содержимое файла
+        var data = await File.ReadAllTextAsync(path);
+        //создаем граф
+        var graph = new List<(int x1, int x2)>();
+
+        //разбиваем содержимое файла на строки и добавляем в граф каждое считанное число
+        var lines = data.Split('\n');
+
+        var firstLine = lines[0].Split(' ');
+        var secondLine = lines[1].Split(' ');
+
+        for (int i = 0; i < firstLine.Length; i++)
+        {
+            graph.Add((int.Parse(firstLine[i]), int.Parse(secondLine[i])));
+        }
+
+        //создаем класс метода кода Прюфера
+        PrueferEncoder encoder = new();
+
+        //Кодирвоанжие графа
+        var code = encoder.Encode(graph);
+
+        //вывод в файл и на консоль
+        StringBuilder sb = new();
+        sb.AppendLine("Код прюфера:");
+
+        code?.ToList().ForEach(x =>
+        {
+            Console.Write(x);
+            sb.Append(x);
+        });
+
+        sb.AppendLine();
+        Console.WriteLine();
+
+        //восстановление графа по коду прюфера
+        var restoredGraph = encoder.Decode(code!);
+
+        //вывод в файл и на консоль
+        sb.AppendLine("Декодированный граф:");
+
+        foreach (var edge in restoredGraph)
+        {
+            sb.Append($"{edge.x1} ");
+            Console.Write($"{edge.x1} ");
+        }
+
+        sb.AppendLine();
+        Console.WriteLine();
+
+        foreach (var edge in restoredGraph)
+        {
+            sb.Append($"{edge.x2} ");
+            Console.Write($"{edge.x2} ");
+        }
+
+        await File.AppendAllTextAsync(path, sb.ToString());
+    }
 }
-
-var json = await File.ReadAllTextAsync(path);
-var graph = JsonConvert.DeserializeObject<List<(int, int)>>(json);
-
-PrueferEncoder encoder = new();
-
-var code = encoder.Encode(graph!);
-
-var settings = new JsonSerializerSettings
-{
-    Formatting = Formatting.Indented
-};
-
-await File.AppendAllTextAsync(path, $"\nКод прюфера: {JsonConvert.SerializeObject(code, settings)}");
-
-var restoredGraph = encoder.Decode(code!);
-
-await File.AppendAllTextAsync(path,$"\nВосстановленный граф: {JsonConvert.SerializeObject(restoredGraph, settings)}");
-
 
 public class PrueferEncoder
 {
-    public IEnumerable<int> Encode(int[,] graph)
-    {
-        if (graph.GetLength(0) != 2)
-        {
-            throw new ArgumentException("Неправильный формат графа.");
-        }
-
-        IList<(int x1, int x2)> graphList = new List<(int x1, int x2)>();
-
-        for (int i = 0; i < graph.GetLength(1); i++)
-        {
-            graphList.Add((graph[0, i], graph[1, i]));
-        }
-
-        return Encode(graphList);
-    }
-
+    //Кодирование графа
     public IEnumerable<int> Encode(IEnumerable<(int x1, int x2)> graph)
     {
         IList<int> prueferCode = new List<int>();
@@ -66,6 +96,7 @@ public class PrueferEncoder
         return prueferCode;
     }
 
+    //Декодирование графа
     public IEnumerable<(int x1, int x2)> Decode(IEnumerable<int> prueferCode)
     {
         IList<(int x1, int x2)> graph = new List<(int x1, int x2)>();
@@ -75,6 +106,7 @@ public class PrueferEncoder
 
         int restoredVertex;
 
+        //Пока не восстановлен весь код прюфера ищем отсутствующую вершину и записываем в список восстановленных
         while (prueferCodeClone.Count > 0)
         {
             restoredVertex = FindMissingVertex(prueferCodeClone, restoredVertexes, vertexCount);
@@ -90,6 +122,7 @@ public class PrueferEncoder
             prueferCodeClone.RemoveAt(0);
         }
 
+        //Восстанавливаем две оставшиеся вершины
         restoredVertex = FindMissingVertex(prueferCodeClone, restoredVertexes, vertexCount);
         restoredVertexes.Add(restoredVertex);
 
@@ -109,6 +142,7 @@ public class PrueferEncoder
         return graph;
     }
 
+    //Вспомогательый метод нахождения минимальной вершины графа для занесения в код.
     private int GetIndexOfMin(IEnumerable<(int x1, int x2)> graph)
     {
         var min = graph.Where(e => !graph.Any(edge => edge.x1 == e.x2)).MinBy(e => e.x2);
@@ -116,6 +150,7 @@ public class PrueferEncoder
         return graph.ToList().FindIndex(v => v == min);
     }
 
+    //Ищет недостающую вершину при восстановлении
     private int FindMissingVertex(IEnumerable<int> prueferCode, IEnumerable<int> restoredVertex, int vertexCount)
     {
         for (int vertex = 1; vertex <= vertexCount; vertex++)
